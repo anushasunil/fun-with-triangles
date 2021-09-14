f75957950560fec92a3cea4845bc4b593e8c4f64
Anusha <anushasunil71201@gmail.com>
2021-09-14 20:21:01 +0530

    docs: added input fields and button

---
+++ index.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelectorAll(".angles");
+const checkButton = document.querySelector(".btn-check");
+const message = document.querySelector(".result");
+
+checkButton.addEventListener("click", function isTriangle()
+{
+    const sumOfAngles = calculateSum(inputs[0].value,inputs[1].value,inputs[2].value )
+    if( sumOfAngles === 180)
+    {
+        console.log("yesssssaaa");
+        message.innerText = "yesssssaaa"
+    }
+    else
+    {
+        console.log("nooooooooooooo");
+        message.innerText = "nooooooooooooo"
+    }
+})
+
+function calculateSum( angle1, angle2, angle3)
+{
+    let sum = Number(inputs[0].value) + Number(inputs[1].value) + Number(inputs[2].value);
+    return sum
+}
