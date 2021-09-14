07201f415f02325c8865a14c2f4c2d5b6ae6dd56
Anusha <anushasunil71201@gmail.com>
2021-09-15 00:01:49 +0530

    added function for calculating hypotenuse

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,3 +1,15 @@
 const sides = document.querySelectorAll(".side-input");
 const hypotenuseButton = document.querySelector(".btn-hypotenuse");
 const outputMessage = document.querySelector(".output-message");
+
+hypotenuseButton.addEventListener("click", function calculateHypotenuse()
+{
+    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value))
+    const hypotenuse = Math.sqrt(sumOfSquares);
+    outputMessage.innerText = "The hypotenuse of the given triangle is " + hypotenuse
+});
+
+function calculateSumOfSquares( a, b)
+{
+    return a**2 + b**2;
+}
