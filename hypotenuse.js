719af2b6b1cf16609abf31dc085b0f699e687f79
Anusha <anushasunil71201@gmail.com>
2021-09-14 23:39:26 +0530

    initial commit

---
+++ hypotenuse.js
@@ -0,0 +1,3 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenuseButton = document.querySelector(".btn-hypotenuse");
+const outputMessage = document.querySelector(".output-message");
