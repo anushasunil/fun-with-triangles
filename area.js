92be6d60da84dc758c64ac36b8591f537780bc86
Anusha <anushasunil71201@gmail.com>
2021-09-15 01:45:12 +0530

    initial commit

---
+++ area.js
@@ -0,0 +1,18 @@
+const baseAndHeight = document.querySelectorAll(".base-and-height");
+const checkButton = document.querySelector(".btn-check");
+const outputMessage = document.querySelector(".output-message");
+
+checkButton.addEventListener("click", function area()
+{
+    const base = Number(baseAndHeight[0].value);
+    const height = Number(baseAndHeight[1].value);
+    const areaOfTriangle = calculateArea(base, height);
+
+    outputMessage.innerText = "The Area of the given triangle is " + areaOfTriangle
+
+})
+
+function calculateArea(base, height)
+{
+    return 0.5*base*height;
+}
